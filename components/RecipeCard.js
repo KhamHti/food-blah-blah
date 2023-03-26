f27a71a4d51f69ad1036a7d7ff5f8409df477825
KhamHti <90329989+KhamHti@users.noreply.github.com>
2023-03-26 23:10:24 +0630

    add thumbnail

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const RecipeCard = ({ recipe }) => {
   const { title, cookingTime, slug, thumbnail } = recipe.fields;
   return (
     <div className="card">
-      <div className="featured">{/* thumbnail image */}</div>
+      <div className="featured">
+        {/* thumbnail image */}
+        <Image
+          src={"https:" + thumbnail.fields.file.url}
+          width={400}
+          height={430}
+
+          //   width={thumbnail.fields.file.details.image.width}
+          //   height={thumbnail.fields.file.details.image.height}
+        />
+      </div>
       <div className="content">
         <div className="info">
           <h3>{title}</h3>
